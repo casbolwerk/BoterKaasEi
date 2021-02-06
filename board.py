6de9b136335518641a1c736d82246e1998d5d5a9
casbolwerk <casbolwerk@hotmail.nl>
2021-02-06 12:31:37 +0100

    add project skeleton

---
+++ board.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+def init_board(n):
+    board = np.zeros((n,n))
+
+    return board
---
+++ main.py
@@ -0,0 +1,10 @@
+from board import init_board
+
+def main():
+    board = init_board(3)
+    print(board)
+    
+    return
+
+if __name__ == '__main__':
+    main()
